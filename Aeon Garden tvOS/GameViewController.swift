66cfb8ba5cc773a9dcab48ff887b31eee5217fd8
amiantos <bradroot@me.com>
2019-03-28 15:50:07 -0700

    cleanup

--- Aeon Garden tvOS/GameViewController.swift
+++ Aeon Garden tvOS/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
+        let scene = AeonTankScene(size: view.bounds.size)
         
         // Present the scene
         let skView = self.view as! SKView
