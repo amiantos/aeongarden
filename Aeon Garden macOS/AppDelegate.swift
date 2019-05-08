bd40f6cc5a493df31808300b2c4bc0919f6c6002
Brad Root <brad@thenounproject.com>
2019-05-08 16:29:58 -0700

    rename some shit

--- Aeon Garden macOS/AppDelegate.swift
+++ Aeon Garden macOS/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "AeonDataModel")
+        let container = NSPersistentContainer(name: "AeonGarden")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error {
                 // Replace this implementation with code to handle the error appropriately.
